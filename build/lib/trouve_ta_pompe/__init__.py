4e19eae3f717c01cd144200d7eca0f1c665fd2ce
rob21runner <kiokio13000@gmail.com>
2023-05-08 18:24:06 +0200

    Delete build/lib/trouve_ta_pompe directory

--- build/lib/trouve_ta_pompe/__init__.py
+++
@@ -1,5 +0,0 @@
-from .map import create_map
-
-
-def show_map():
-    create_map()
--- build/lib/trouve_ta_pompe/cli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from trouve_ta_pompe import show_map
-
-def main():
-    show_map()
-
-if __name__ == "__main__":
-    main()
